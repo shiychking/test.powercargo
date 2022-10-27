c7ce73c371f10d2712d0c7ba71d2d34ad8a31798
shiychking <13910032477@139.com>
2022-10-27 20:24:09 +0800

    ,

--- src/test/powercargo/stringLearn.java
+++ src/test/powercargo/stringLearn.java
@@ -6,7 +6,7 @@ public class stringLearn {
         System.out.println(10-1>=9);
 		float[][]  myFloats =new float[][]{{0.0f,0.1f,0.2f},{0.03f,0.4f,0.5f},{0.6f,0.7f,0.8f},{0.9f,1.0f,1.1f}};
 		for(int index= 0 ;index< myFloats.length;index++) {
-			System.out.println(String.format("第%d行数，刚添加的信息", index));
+			System.out.println(String.format("第%d行数", index));
 			for(int index1=0;index1<myFloats[index].length;index1++) {
 				
 			    System.out.println(myFloats[index][index1]);			
